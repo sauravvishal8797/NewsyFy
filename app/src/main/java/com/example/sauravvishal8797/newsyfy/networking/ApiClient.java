fb8c350cd830b270e4c752eef04efbcd6375d4e8
sauravvishal8797 <sauravvishal8797@gmail.com>
2019-03-04 13:10:25 +0530

    Implemented Offline caching with retrofit

--- app/src/main/java/com/example/sauravvishal8797/newsyfy/networking/ApiClient.java
+++ app/src/main/java/com/example/sauravvishal8797/newsyfy/networking/ApiClient.java
@@ -1,5 +1,18 @@
 package com.example.sauravvishal8797.newsyfy.networking;
 
+import android.content.Context;
+import android.net.NetworkInfo;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,13 +21,95 @@ public class ApiClient {
     public static final String BASE_URL = "https://newsapi.org/v2/";
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient() {
+    //Offline cache
+    public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    public static final String HEADER_PRAGMA = "Pragma";
+
+    public static Retrofit getClient(Context context) {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(OfflineCacheInterceptor(context))
+                .addNetworkInterceptor(CacheInterceptor(context))
+                .cache(offlineCache(context))
+                .build();
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;
+    }
+
+    /**Creates cache of size 20 Mb
+     * @return Cache object
+     * */
+    private static Cache offlineCache(Context context) {
+        Cache cache = null;
+        try {
+            cache = new Cache(new File(context.getCacheDir(), "http-cache"),
+                    20 * 1024 * 1024);
+        } catch (Exception e) {
+
+        }
+        return cache;
+    }
+
+    private static Interceptor CacheInterceptor(Context context) {
+        return chain -> {
+            Response response = chain.proceed(chain.request());
+            CacheControl cacheControl;
+            if (isConnectedToNetwork(context)) {
+                cacheControl = new CacheControl.Builder()
+                        .maxAge(0, TimeUnit.SECONDS)
+                        .build();
+            } else {
+                cacheControl = new CacheControl.Builder()
+                        .maxStale(7, TimeUnit.DAYS)
+                        .build();
+            }
+
+            return response.newBuilder()
+                    .removeHeader(HEADER_PRAGMA)
+                    .removeHeader(HEADER_CACHE_CONTROL)
+                    .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                    .build();
+
+        };
+    }
+
+    private static Interceptor OfflineCacheInterceptor(Context context) {
+        return chain -> {
+            Request request = chain.request();
+
+            if (!isConnectedToNetwork(context)) {
+                CacheControl cacheControl = new CacheControl.Builder()
+                        .maxStale(7, TimeUnit.DAYS)
+                        .build();
+
+                request = request.newBuilder()
+                        .removeHeader(HEADER_PRAGMA)
+                        .removeHeader(HEADER_CACHE_CONTROL)
+                        .cacheControl(cacheControl)
+                        .build();
+            }
+            return chain.proceed(request);
+        };
+    }
+
+    public static boolean isConnectedToNetwork(Context context) {
+        boolean isConnected = false;
+        try {
+            android.net.ConnectivityManager connectivityManager = (android.net.ConnectivityManager) context.getSystemService(
+                    Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        } catch (Exception e) {}
+        return isConnected;
     }
 }
